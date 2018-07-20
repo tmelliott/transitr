d300eaf6c84b30b80347c9b6ed819edcb6b47575
Tom Elliott <tomelliottnz@gmail.com>
2018-07-20 16:51:25 +1200

    Add libcurl to dependencies

--- src/realtime_main.cpp
+++ src/realtime_main.cpp
@@ -36,7 +36,22 @@ void run_realtime_model (
     CURLcode res;
     curl_global_init (CURL_GLOBAL_DEFAULT);
     curl = curl_easy_init ();
-    
+    if (curl)
+    {
+        curl_easy_setopt (curl, CURLOPT_URL, url.c_str ());
+        res = curl_easy_perform (curl);
+        if (res == CURLE_OK)
+        {
+            Rcout << "OK!\n";
+        }
+        else
+        {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+        }
+        curl_easy_cleanup (curl);
+    }
+    curl_global_cleanup ();
     
     // Connect GTFS database
     Gtfs gtfs (dbname);
