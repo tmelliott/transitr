aa5c2dd36ca44b7caba55e542cd425d72e863523
Tom Elliott <tomelliottnz@gmail.com>
2018-07-20 16:43:28 +1200

    Use CURL

--- R/model.R
+++ R/model.R
@@ -4,11 +4,11 @@ model <- function(nw, n.particles = 500, cores = 1L) {
     if (!check_tables(nw)) stop("GTFS tables don't appear to be valid.")
 
     if (is.null(nw$apis$realtime)) stop("Please attach a realtime API feed, `realtime_feed()`")
-    x <- send(nw$apis$realtime)
-    if (x$status != 200) {
-        print(x)
-        stop("Unable to access the API feed - have you added the key?")
-    }
+    # x <- send(nw$apis$realtime)
+    # if (x$status != 200) {
+    #     print(x)
+    #     stop("Unable to access the API feed - have you added the key?")
+    # }
 
     run_realtime_model(nw, n.particles, cores)
 }
--- src/realtime_main.cpp
+++ src/realtime_main.cpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <Rcpp.h>
+#include <curl/curl.h>
 
 #include "vendor/sqlite3/sqlite3.h"
 
@@ -19,12 +20,28 @@ void run_realtime_model (
     int nparticles,
     int numcore)
 {
-    std::string dbname;
-    {
-        String dbname_raw = nw["database"];
-        dbname = dbname_raw;
-    }
+    // Process nw components into c++ things
+    String dbname_raw = nw["database"];
+    std::string dbname (dbname_raw);
+    // std::string dbname (get_database_name (nw));
+
+    List apis = nw["apis"];
+    List rt = apis["realtime"];
+    String url_raw = rt["url"];
+    std::string url (url_raw);
+    // std::string url (get_feed_url (nw));
+
+    Rcout << "Oh look, it's the API url! " << url << "\n";
+    CURL *curl;
+    CURLcode res;
+    curl_global_init (CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init ();
+    
+    
+    // Connect GTFS database
     Gtfs gtfs (dbname);
 
-    
+
+
+
 }
