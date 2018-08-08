13c80155f01bede3e32d41eb70a2afdbc2a30870
Tom Elliott <tomelliottnz@gmail.com>
2018-08-08 15:44:20 +1200

    Convert timestamp from string to a timestamp for postgres

--- scripts/copy_to_postgres.R
+++ scripts/copy_to_postgres.R
@@ -8,7 +8,7 @@ if (HOST != "") {
     vps <- dbReadTable(c1, "vehicles")
     dbDisconnect(c1)
 
-    vps$timestamp <- as.integer(vps$timestamp)
+    vps$timestamp <- as.POSIXct(as.integer(vps$timestamp), origin = "1970-01-01")
 
     c2 <- dbConnect(PostgreSQL(),
                     user = "tell029", host = HOST, dbname = "realtime")
