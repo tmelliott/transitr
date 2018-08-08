94b63690623d1258c309a113b583ad1f3a2b8511
Tom Elliott <tomelliottnz@gmail.com>
2018-08-08 14:53:07 +1200

    SQL commands for database

---
+++ scripts/create_postgres_tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE vehicles (
+    vehicle_id VARCHAR(30) PRIMARY KEY NOT NULL,
+    trip_id VARCHAR(80),
+    timestamp TIMESTAMP,
+    position_latitude REAL,
+    position_longitude REAL,
+    distance REAL,
+    speed REAL,
+    progress SMALLINT
+)
