72a3b5ebb038723b8df48315258be81b38ca90bb
Tom Elliott <tomelliottnz@gmail.com>
2018-08-23 17:12:28 +1200

    Begin constructing network via segmentation
    - remove stops from segments (these cause issues with matching)
    - find segments of a shape that match another

--- R/network_construct.R
+++ R/network_construct.R
@@ -27,3 +27,26 @@ tmp_construct_network <- function(nw) {
     RSQLite::dbWriteTable(con, "shape_segments", sh_segs, append = TRUE)
     db_close(con)
 }
+
+
+merge_paths <- function(new, existing) {
+    ## calculate distance from point in new to nearest point in existing
+    ds <- apply(new, 1, geosphere::dist2Line, line = existing)
+    ds[1, ]
+}
+
+delete_stops <- function(path, stops) {
+    d <- apply(path[, c("shape_pt_lon", "shape_pt_lat")] %>% as.matrix, 1, 
+        function(x) min(geosphere::distGeo(x, p2 = stops)))
+    for (i in 1:(length(d)-1)) if (d[i+1] < 2) d[i] <- NA
+    path <- path[!is.na(d), ]
+    d <- d[!is.na(d)]
+    # interpolate 
+    for (i in which(d == 0)) {
+        if (i == 1 || i == length(d)) next
+        path[i, c("shape_pt_lon", "shape_pt_lat")] <- 
+            geosphere::dist2Line(path[i, c("shape_pt_lon", "shape_pt_lat")], 
+                path[c(i-1, i+1), c("shape_pt_lon", "shape_pt_lat")] %>% as.matrix)[, 2:3]
+    }
+    path
+}
---
+++ scripts/develop_network.R
@@ -0,0 +1,56 @@
+library(tidyverse)
+library(RSQLite)
+library(dbplyr)
+
+db <- "fulldata.db"
+con <- dbConnect(SQLite(), db)
+
+routes <- con %>% tbl("routes") %>%
+    filter(route_short_name %in% c("982", "983") &
+           route_long_name %like% "Gulf Harbour To HC Station%" &
+           version == 68.11) %>%
+    inner_join(con %>% tbl("trips"), by = "route_id") %>%
+    select(route_id, route_short_name, route_long_name, shape_id) %>%
+    distinct %>% collect
+
+shapes <- con %>% tbl("shapes") %>%
+    filter(shape_id %in% routes$shape_id) %>%
+    arrange(shape_id, shape_pt_sequence) %>%
+    select(shape_id, shape_pt_lat, shape_pt_lon, shape_pt_sequence) %>%
+    collect
+
+latr <- extendrange(shapes$shape_pt_lat)
+lonr <- extendrange(shapes$shape_pt_lon)
+stops <- con %>% tbl("stops") %>%
+    filter(stop_lat > latr[1] & stop_lat < latr[2] &
+           stop_lon > lonr[1] & stop_lon < lonr[2] &
+           version == 68.11) %>%
+    select(stop_lon, stop_lat) %>%
+    collect
+
+ggplot(shapes, aes(shape_pt_lon, shape_pt_lat, 
+                   group = shape_id, colour = shape_id)) +
+    geom_path()
+
+
+
+
+## should end up with 15 segments; 12 nodes
+path1 <- shapes %>% 
+    filter(shape_id == routes$shape_id[routes$route_short_name == "983"]) %>%
+    arrange(shape_pt_sequence) %>% 
+    delete_stops(stops)
+path2 <- shapes %>% 
+    filter(shape_id == routes$shape_id[routes$route_short_name == "982"]) %>%
+    arrange(shape_pt_sequence) %>% 
+    delete_stops(stops)
+
+existing <- path1[, 3:2]
+new <- path2[, 3:2]
+
+ds <- merge_paths(new, existing)
+ggplot(NULL, aes(seq_along(ds), ds, color = ds < 5)) + geom_point()
+
+ggplot(path1, aes(shape_pt_lon, shape_pt_lat)) +
+    geom_path() +
+    geom_point(aes(colour = d < 5), data = path2 %>% mutate(d = ds) %>% arrange(d))
