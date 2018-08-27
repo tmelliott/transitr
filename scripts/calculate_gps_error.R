2c82fbabcbcee05eac2ee8e742909032054ec08b
Tom Elliott <tomelliottnz@gmail.com>
2018-08-27 17:38:18 +1200

    Fetch vps and calculate distances from route

--- scripts/calculate_gps_error.R
+++ scripts/calculate_gps_error.R
@@ -1,12 +1,39 @@
 library(tidyverse)
+library(RProtoBuf)
 library(RSQLite)
 library(dbplyr)
+library(pbapply)
+readProtoFiles("src/vendor/protobuf/gtfs-realtime.proto")
 
-con <- dbConnect(SQLite(), "fulldata.db")
-vps <- con %>% tbl("vehicles") %>% filter(progress > 0 & progress < 100) %>% collect
-dbDisconnect(con)
+## Fetch the archive from the r-pi
+if (FALSE) {
+    date <- as.POSIXct("2018-08-24")
+    system(sprintf("scp tom@130.216.51.230:/mnt/storage/history/%s/%s/%s/archive_%s.zip archive.zip",
+                   format(date, "%Y"), format(date, "%m"), format(date, "%d"),
+                   format(date, "%Y_%m_%d")))
+}
 
-ggplot(vps, aes(position_longitude, position_latitude)) +
+## Extract the contents and read into memory
+files <- unzip("archive.zip", exdir = tempdir())
+
+vfiles <- files[grepl("vehicle_locations", files)]
+vps <- do.call(bind_rows, pblapply(vfiles[1:100], function(f) {
+    feed <- read(transit_realtime.FeedMessage, f)
+    do.call(bind_rows, lapply(feed$entity, function(x) {
+        if (!x$has('vehicle') ||
+            !x$vehicle$has('vehicle') ||
+            !x$vehicle$has('trip') ||
+            !x$vehicle$has('position')) return(NULL)
+        tibble(vehicle_id = x$vehicle$vehicle$id,
+               timestamp = x$vehicle$timestamp,
+               trip_id = x$vehicle$trip$trip_id,
+               route_id = x$vehicle$trip$route_id,
+               lon = x$vehicle$position$longitude,
+               lat = x$vehicle$position$latitude)
+    }))
+}))
+
+ggplot(vps, aes(lon, lat)) +
     geom_point() +
     coord_fixed(1.2)
 
@@ -20,14 +47,21 @@ for (tid in unique(vps$trip_id)) {
     
     sgeom <- sf::st_linestring(shape %>% select(shape_pt_lon, shape_pt_lat) %>% as.matrix)
     vps$dist2line[vps$trip_id == tid] <- 
-        geosphere::dist2Line(vps[vps$trip_id == tid, c("position_longitude", "position_latitude")], sgeom)[,1]
+        geosphere::dist2Line(vps[vps$trip_id == tid, c("lon", "lat")], sgeom)[,1]
 
-    p <- ggplot(shape, aes(shape_pt_lon, shape_pt_lat)) + geom_path() + coord_fixed(1.6) +
-        geom_label(aes(position_longitude, position_latitude, label = round(dist2line)), data = vps[vps$trip_id == tid, ])
-        # geom_point(aes(position_longitude, position_latitude), data = vps[vps$trip_id == tid, ])
-    print(p)
-    grid::grid.locator()
+    # p <- ggplot(shape, aes(shape_pt_lon, shape_pt_lat)) + geom_path() + coord_fixed(1.6) +
+    #     geom_label(aes(lon, lat, label = round(dist2line)), data = vps[vps$trip_id == tid, ])
+    #     # geom_point(aes(lon, lat), data = vps[vps$trip_id == tid, ])
+    # print(p)
+    # grid::grid.locator()
 }
 
-d <- vps$dist2line[vps$dist2line < 100 & vps$dist2line > 0]
-hist(d, 100)
+d <- vps$dist2line[vps$dist2line < 9 & vps$dist2line >= 0]
+
+sig2 <- 3^2
+hist(d^2 / sig2, 100, freq=F)
+curve(dexp(x, 2), 0, 15, col="red", add=T)
+
+xx <- rnorm(1000)^2 + rnorm(1000)^2
+hist(xx, 100, freq=F)
+curve(dexp(x, 0.5), 0, 15, col="red", add=T)
