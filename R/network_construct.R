3583b1d638e5fdc27976526ea26432b86c6670cf
Tom Elliott <tomelliottnz@gmail.com>
2018-09-24 15:43:52 +1200

    start creating segments between stops

--- R/network_construct.R
+++ R/network_construct.R
@@ -1,3 +1,76 @@
+construct_network <- function(nw) {
+    con <- db_connect(nw$database)
+
+    intersections <- dbReadTable(con, "intersections")
+    segments <- dbReadTable(con, "road_segments")
+    shape_segments <- dbReadTable(con, "shape_segments")
+
+    ## For each route ...
+    routes <- dbGetQuery(con, "SELECT route_id FROM routes")
+    trq <- dbSendQuery(con, "SELECT trip_id, shape_id FROM trips WHERE route_id=? LIMIT 1")
+    stq <- dbSendQuery(con, "SELECT stop_times.stop_id, stop_sequence, stop_lat, stop_lon FROM stop_times, stops WHERE trip_id=? AND stop_times.stop_id=stops.stop_id ORDER BY stop_sequence")
+    shq <- dbSendQuery(con, "SELECT shape_pt_lat, shape_pt_lon, shape_pt_sequence FROM shapes WHERE shape_id=? ORDER BY shape_pt_sequence")
+
+    pb <- txtProgressBar(0, nrow(routes), style = 3)
+    for (route in routes$route_id) {
+        pb$up(pb$getVal() + 1)
+        dbBind(trq, list(route))
+        res <- dbFetch(trq)
+        dbClearResult(trq)
+
+        ## ... get a trip and find stop sequence ...
+        dbBind(stq, list(res$trip_id))
+        stops <- dbFetch(stq)
+        dbClearResult(stq)
+
+        ## ... and get the shape ...
+        dbBind(shq, list(res$shape_id))
+        shape <- dbFetch(shq)
+        dbClearResult(shq)
+
+        ## ... then create "intersections" at stops ... 
+        intid = NA
+        segs <- integer(nrow(stops) - 1)
+        for (i in 1:nrow(stops)) {
+            previd <- intid
+            si <- stops[i, ]
+            inti <- intersections[intersections$intersection_lat == si$stop_lat & 
+                                  intersections$intersection_lon == si$stop_lon, ]
+            if (nrow(inti) == 0) {
+                ## create new intesection
+                intid <- max(1, max(intersections$intersection_id) + 1)
+                intersections <- rbind(intersections, 
+                    data.frame(intersection_id = max(intersections$intersection_id)+1,
+                               intersection_lat = si$stop_lat,
+                               intersection_lon = si$stop_lon))
+            } else {
+                ## use existing
+                intid <- inti$intersection_id[1]
+            }
+
+            if (i > 1) {
+                ## ... and segments between intersections.
+                segi <- segments[segments$int_from == previd & segments$int_to == intid, ]
+                if (nrow(segi) == 0) {
+                    ## create new segment
+                    segid <- max(1, max(segments$road_segment_id) + 1)
+                    segments <- rbind(segments,
+                        data.frame(road_segment_id = segid, int_from = previd, int_to = intid, length = 0))
+                } else {
+                    ## use existing
+                    segid <- segi$road_segment_id
+                }
+                segs[i-1] <- segid
+            }
+        }
+
+        ## Finally, create shape_segments
+
+    }
+
+    db_close(con)
+}
+
 tmp_construct_network <- function(nw) {
     shapes <- load_shapes(nw)
 
