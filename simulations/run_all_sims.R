d7e1b642766d069da7c469fde247eac7c6a3f167
Tom Elliott <tomelliottnz@gmail.com>
2018-10-23 09:04:53 +1300

    only do sims that are new

--- simulations/run_all_sims.R
+++ simulations/run_all_sims.R
@@ -1,5 +1,7 @@
 simdirs <- list.files(pattern = "sim_")
 
-for (simdir in simdirs)
+for (simdir in simdirs) {
+    if (file.exists(file.path(simdir, "timings.csv"))) next()
     system(sprintf("cd ~/transitr && make simulation SIM=%s", simdir))
+}
 
