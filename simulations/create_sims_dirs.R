a81c0c77fb455bb441bf8553fe09c847dfb832ef
Tom Elliott <tomelliottnz@gmail.com>
2018-10-23 09:03:20 +1300

    Remove simdirs from VC

--- simulations/create_sims_dirs.R
+++ simulations/create_sims_dirs.R
@@ -1,4 +1,4 @@
-n <- c(1000, 2000, 4000, 8000)
+n <- c(500, 1000, 2000, 4000, 8000)
 gps <- c(2, 3, 5, 8)
 noise <- c(0.001, 0.005, 0.01, 0.02, 0.05, 0.1)
 
@@ -14,13 +14,20 @@ config <- list(
     )
 
 grid <- expand.grid(n = n, gps = gps, noise = noise)
-apply(grid, 1, function(x) {
+dirs <- apply(grid, 1, function(x) {
     config$n_particles <- x[["n"]]
     config$gps_error <- x[["gps"]]
     config$system_noise <- x[["noise"]]
     dirname <- glue::glue("sim_{config$n_particles}_{config$gps_error}_{config$system_noise}")
     dirname <- gsub("\\.", "-", dirname)
-    if (dir.exists(dirname)) unlink(dirname, TRUE, TRUE)
+    if (dir.exists(dirname)) return(dirname)
     dir.create(dirname)
     jsonlite::write_json(config, file.path(dirname, "config.json"), auto_unbox = TRUE, pretty = TRUE)
+    dirname
 })
+
+existing <- list.files(pattern = 'sim_', include.dirs = TRUE)
+todelete <- existing[!existing %in% dirs]
+sapply(todelete, unlink, recursive = TRUE, force = TRUE)
+
+
