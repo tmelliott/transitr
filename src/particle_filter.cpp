2eaa1f1be794659bae1bc62bfb8eba1aa76823a8
Tom Elliott <tomelliottnz@gmail.com>
2018-12-06 16:10:35 +1300

    debug text

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -233,9 +233,12 @@ namespace Gtfs {
                     << _current_segment << " to stop ";
                 std::cout.flush ();
                 int m = find_stop_index (dmin, &(_trip->stops ()));
-                std::cout << m;
+                std::cout << m << "; of "
+                    << segs.size () << " segments, on segment ";
                 std::cout.flush (); 
+
                 int l = find_segment_index (dmin, &segs);
+                std::cout << l;
                 std::cout.flush ();
 
                 // update segment travel times for intermediate ones ...
