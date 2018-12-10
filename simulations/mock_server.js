e9f044a0e04df78fb4babcfaae98da0a0c208cc6
Tom Elliott <tomelliottnz@gmail.com>
2018-12-10 15:57:01 +1300

    Allow simulation to start from a given time for easier debugging

--- simulations/mock_server.js
+++ simulations/mock_server.js
@@ -17,6 +17,41 @@ var pids = [] // {"pid001": 1}
 
 app.use('/archive', express.static('archive'))
 
+app.use("/:pid/:ts/minutes/:min/vehicle_positions", (req, res) => {
+    const pid = req.params.pid
+    var ts = parseInt(req.params.ts)
+    const min = parseInt(req.params.min)
+    ts -= min*60
+    ts *= 1000
+    var x = new Date()
+    x.setTime(ts)
+    const d = x.getFullYear() + 
+        (x.getMonth() < 9 ? 0 : '') + (x.getMonth() + 1) + 
+        (x.getDate() < 10 ? 0 : '') + x.getDate() +
+        (x.getHours() < 10 ? 0 : '' ) + x.getHours() +
+        (x.getMinutes() < 10 ? 0 : '' ) + x.getMinutes() +
+        (x.getSeconds() < 10 ? 0 : '' ) + x.getSeconds()
+    const dt = parseInt(d)
+    var start = 0
+    for (var i=0; i<files.length; i++) {
+        if (parseInt(files[i].replace(/[a-z_\.]*/g, '')) > dt) {
+            start = i-1
+            break
+        }
+    }
+    if (!(pid in pids)) pids[pid] = 0
+    if (pids[pid] < start) {
+        console.log('starting at ' + start)
+        pids[pid] = start
+    }
+    if (pids[pid] >= files.length) {
+        res.send('simulation complete')
+        return
+    }
+    res.download(path.join(__dirname, 'archive/' + files[pids[pid]]));
+    pids[pid]++
+})
+
 app.use('/:pid/:start/:end/vehicle_positions', (req, res) => {
     const pid = req.params.pid
     const start = parseInt(req.params.start)
