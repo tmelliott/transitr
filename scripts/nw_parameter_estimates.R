4938aaa4266634561b81bfdb805d21c7e09c1930
Tom Elliott <tomelliottnz@gmail.com>
2019-01-14 17:08:52 +1300

    NW model for indep segments

--- scripts/nw_parameter_estimates.R
+++ scripts/nw_parameter_estimates.R
@@ -32,34 +32,130 @@ nwobs <-
     )
 
 ## look at the data
-seg <- nwobs$segment_id %>% table %>% sort %>% names %>% tail(20)
+seg <- nwobs$segment_id %>% table %>% sort %>% names %>% tail(10)
 
 ps <- ggplot(nwobs %>% filter(segment_id %in% seg)) +
-    facet_wrap(~segment_id, scales = "free") +
-    scale_x_time()
+    facet_wrap(~segment_id)
 
-ggplot(nwobs, aes(timestamp, speedkm)) + 
-    geom_point(alpha = 0.05) +
-    geom_quantile(quantiles = 0.5)
+# ggplot(nwobs, aes(timestamp, speedkm)) + 
+#     geom_point(alpha = 0.05) +
+#     geom_quantile(quantiles = 0.5)
 
-ps + geom_histogram(aes(travel_time))
+ps + geom_histogram(aes(travel_time)) + scale_x_continuous(trans = "log")
 ps + geom_histogram(aes(speedkm)) + xlim(0, 100)
 
-ps + geom_point(aes(timestamp, travel_time))
+ps + geom_path(aes(timestamp, travel_time)) #+ scale_y_log10()
 ps + geom_point(aes(timestamp, speedkm)) + ylim(0, 100)
 
 
 ## which time period does each obs belong?
-times <- pretty(nwobs$timestamp, 20)
+times <- pretty(nwobs$timestamp, 20 * 4)
 nwobsf <- nwobs %>%
-    mutate(period = cut(nwobs$timestamp, breaks = times)) %>%
+    filter(segment_id %in% seg) %>%
+    mutate(period = cut((.)$timestamp, breaks = times)) %>%
     group_by(segment_id, period) %>%
-    summarize(travel_time = median(travel_time), length = first(length)) %>%
+    summarize(
+        time = mean(travel_time, na.rm = TRUE),
+        error = sd(travel_time, na.rm = TRUE),
+        log_time = mean(log(travel_time), na.rm = TRUE),
+        log_error = sd(log(travel_time), na.rm = TRUE),
+        n = n(),
+        length = first(length)
+    ) %>%
+    ungroup () %>%
     mutate(
-        speed = length / travel_time,
+        speed = length / exp(time),
         speedkm = speed / 1000 * 60 * 60
+    ) %>%
+    arrange(segment_id, period) %>%
+    mutate(period_int = as.integer(period))
+
+Ymat <- nwobsf %>% 
+    select(segment_id, period_int, time) %>% 
+    spread(key = period_int, value = time) %>%
+    select(-1) %>% as.matrix
+Yerr <- nwobsf %>% 
+    select(segment_id, period_int, error) %>%
+    spread(key = period_int, value = error) %>%
+    select(-1) %>% as.matrix
+
+ggplot(nwobsf) +
+    geom_path(aes(period_int, time)) +
+    facet_wrap(~segment_id)
+
+mean(apply(Ymat, 1, function(x) sd(diff(x), na.rm = TRUE)), na.rm = TRUE) ## Sigma_mu
+sd(apply(Ymat, 1, function(x) sd(diff(x), na.rm = TRUE)), na.rm = TRUE)   ## Sigma_sig
+
+nwdata <- 
+    list(
+        M = nrow(Ymat),
+        T = ncol(Ymat),
+        Y = Ymat
     )
 
-ggplot(nwobsf, aes(period, speedkm)) + 
-    geom_path() +
-    facet_wrap(~segment_id)
+jags.fit <- 
+    jags.model(
+        'scripts/nw_model_indep.jags', 
+        nwdata,
+        n.adapt = 1000,
+        n.chains = 1
+    )
+
+samps <- 
+    coda.samples(
+        jags.fit, 
+        c('Q_mu', 'Q_sig', 'R_mu', 'R_sig'), 
+        n.iter = 1000, 
+        thin = 1
+    )
+
+summary(samps)
+plot(samps)
+
+# sds <- coda.samples(jags.fit, 'sig_r', n.iter = 10000, thin = 10)
+# summary(sds)
+
+# Sigmas <- coda.samples(jags.fit, 'Sigma', n.iter = 10000, thin = 10)
+# summary(Sigmas)
+
+
+Xs <- coda.samples(jags.fit, 'X', n.iter = 1000, thin = 1)
+
+xdf <- 
+    expand.grid(
+        segment = 1:nwdata$M, 
+        time = as.POSIXct(as.character(unique(nwobsf$period)), origin = "1970-01-01")
+    ) %>% 
+    as.tibble()
+for (i in sample(1:10)) {
+    X1 <- matrix(as.array(Xs)[i,], nrow = nwdata$M)
+    X1df <- xdf %>% mutate(estimate = as.numeric(X1), obs = as.numeric(Ymat))
+
+    g <- ggplot(X1df) + 
+        geom_point(aes(time, obs)) + 
+        geom_path(aes(time, estimate), colour = 'red') + 
+        facet_wrap(~segment)
+    dev.hold()
+    print(g)
+    dev.flush()
+}
+
+Xds <- coda.samples(jags.fit, 'Xdot', n.iter = 1000, thin = 1)
+
+xdf <- 
+    expand.grid(
+        segment = 1:nwdata$M, 
+        time = as.POSIXct(as.character(unique(nwobsf$period)), origin = "1970-01-01")
+    ) %>% 
+    as.tibble()
+for (i in sample(1:10)) {
+    Xd1 <- matrix(as.array(Xds)[i,], nrow = nwdata$M)
+    Xd1df <- xdf %>% mutate(estimate = as.numeric(Xd1))
+
+    g <- ggplot(Xd1df) + 
+        geom_path(aes(time, estimate), colour = 'red') + 
+        facet_wrap(~segment) + ylim(range(as.matrix(Xds)))
+    dev.hold()
+    print(g)
+    dev.flush()
+}
