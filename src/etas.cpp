4fda1335f6e78c7ce3fa071aec857fad5554603e
Tom Elliott <tomelliottnz@gmail.com>
2019-10-03 10:40:07 +1300

    Code

--- src/etas.cpp
+++ src/etas.cpp
@@ -14,6 +14,19 @@ namespace Gtfs {
     {
         // Update trip state
         if (!loaded) load ();
+
+        std::cout << "\n * Updating trip " << _trip_id;
+
+        int eta_model (gtfs->parameters ()->eta_model);
+        std::cout
+            << "\n   - Estimating ETAs using Model " << eta_model;
+
+        std::cout << "\n   - Vehicle: "
+            << (_vehicle == nullptr ? "none" : _vehicle->vehicle_id ());
+
+
+        std::cout << "\n\n------------------------------------------\n";
+
     }
 
     etavector Trip::get_etas ()
--- src/test-etas.cpp
+++ src/test-etas.cpp
@@ -1,12 +1,26 @@
 #include <testthat.h>
+#include <iostream>
+#include <string>
 
-#include "time.h"
+#include "gtfs.h"
 
 context("Arrival time estimation") {
     // first need GTFS object
-    
+    std::string dbname ("auckland_gtfs.db");
+    Gtfs::Gtfs gtfs (dbname);
+    Gtfs::par par;
+    par.nw_measurement_error = 5;
+    RNG rng (10);
+
     test_that("Trip states initialized correctly") {
         // create a trip and initialize it .. 
-        
+        std::string t ("1141160875-20190613111133_v80.31");
+        Gtfs::Trip* t0 = gtfs.find_trip (t);
+
+        uint64_t ts = 1562034647;
+
+        t0->update (ts, rng);
+
+        expect_true (0);
     }
 }
