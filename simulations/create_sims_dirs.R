9fb622d42f7012b079d6d38dd3645e5571a59fe4
Tom Elliott <tomelliottnz@gmail.com>
2019-10-11 10:03:38 +1300

    dir stuff

--- simulations/create_sims_dirs.R
+++ simulations/create_sims_dirs.R
@@ -1,12 +1,12 @@
 config <- list(
     n_core = 4,
-    n_particles = 2000,
-    noise_model = 1,
-    system_noise = c(0.6, 0.7, 0.8, 0.9),
+    n_particles = c(500, 1000),#, 2000, 5000, 10000),
+    noise_model = 0,
+    system_noise = c(0.1, 0.2),#, 0.4, 0.6, 0.8, 1.0),
     pr_stop = 0.1,
     dwell_time = 6.0,
     gamma = 5.0,
-    gps_error = 3,
+    gps_error = 2:3,#c(1, 2, 3, 5),
     arrival_error = 5,
     departure_error = 5,
     nw_system_noise = 0.001,
@@ -18,7 +18,7 @@ grid <- do.call(expand.grid, config)
 
 for (i in 1:nrow(grid)) {
     conf <- grid[i, , drop = TRUE]
-    dirname <- glue::glue("sim_334{conf$noise_model}-{conf$system_noise}")
+    dirname <- glue::glue("sim_august_{conf$n_particles}-{conf$system_noise}-{conf$gps_error}")
     if (!dir.exists(dirname))
         dir.create(dirname)
     if (!file.exists(file.path(dirname, "config.json")))
